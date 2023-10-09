2c22b1606d17994362b869e2b4c34d56527c48ea
Carolyn Pierce <cpierce@unc.edu>
2023-10-09 17:38:32 -0400

    fixex04again

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,15 +2,12 @@
 
 __author__ = "730477957"
 
-# Importing randint to generate random numbers. 
-from random import randint
-
 
 # Defining a function named all that checks numbers at each index. 
 def all(list_numbers: list[int], number: int) -> bool: 
     """Returns True if all numbers match at indicated number."""
 # If the length of the list is empty, returns False. 
-    if len(list_numbers) <= 0:
+    if len(list_numbers) == []:
         return False 
     idx: int = 0
 # Checks for number mathes at each index. 
@@ -24,14 +21,14 @@ def all(list_numbers: list[int], number: int) -> bool:
 # Defining a fucntion max to find the highest number when provided a list of numbers. 
 def max(list_num2: list[int]) -> int:
     """Returns highest integer."""
-# raises an error if the list is empty. 
+# Raises an error if the list is empty. 
     if len(list_num2) == 0:
         raise ValueError("max() arg is an empty List")
 # Creating a variable to store the max number and searching for the max number. 
     highest_number: int = list_num2[0]
     idx: int = 1
     while idx < len(list_num2):
-        if highest_number < len(list_num2): 
+        if highest_number < list_num2[idx]: 
             highest_number = list_num2[idx]
         idx += 1
     return highest_number
@@ -40,14 +37,13 @@ def max(list_num2: list[int]) -> int:
 # Defining a fucntion called is_equal to check if the list fo numbers are the same in each index. 
 def is_equal(list_num3: list[int], list_num4: list[int]):
     """Returns True if every element at each index is equal in both lists."""
-    idx: int = 0
 # If the lengths of the list do not match, returns False. 
     if len(list_num3) != len(list_num4):
         return False
+    idx: int = 0
 # Checks if lists match at each index, only returns True if the lists match at every indice. 
-    while idx < len(list_num3): 
+    while idx < len(list_num3) and len(list_num4): 
         if list_num3[idx] != list_num4[idx]:
-            idx += 1
-        else: 
-            return False 
+            return False
+        idx += 1 
     return True
