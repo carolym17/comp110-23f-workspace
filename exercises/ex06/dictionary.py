41350250102a98f7afdeeb7dbaac238acaee6b67
Carolyn Pierce <cpierce@unc.edu>
2023-10-27 23:38:58 -0400

    editex06

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -8,7 +8,7 @@ def invert(input: dict[str, str]) -> dict[str, str]:
     # Create an empty dictionary. 
     inverted = dict[str, str] = dict()
     # Iterate over the key, value pairs of dictionary x. 
-    for key in input():
+    for key in input:
         # If there is a dumplicate, raise KeyError. 
         if input[key] in inverted:
             raise KeyError("KeyError: There is a duplicate key.")
@@ -18,43 +18,41 @@ def invert(input: dict[str, str]) -> dict[str, str]:
     return inverted
 
 
-def favorite_colors(input_1: dict[str, str]) -> str:
+def favorite_color(input: dict[str, str]) -> str:
     """Returns favorite color- the color that appears most frequently."""
-    # Create empty string to return favorite color. 
-    most_popular_color: str = ""
     # Create empty dictionary
     colors_list: list[str] = list()
-    max_number: int = 0 
     # Iterate through names and favorite colors. 
     for name, color in input:
         colors_list.append[input[name]]
     # Count the times color appears. 
-    number_of_time_color_appeared: dict[str, int] = ()
-    for color in number_of_time_color_appeared:
-        if color in number_of_time_color_appeared:
-            number_of_time_color_appeared[color] += 1
-        else: number_of_time_color_appeared[color] = 1
-        return number_of_time_color_appeared
+    color_counter_dict: dict[str, int] = dict()
+    for color in colors_list:
+        if color in color_counter_dict:
+            color_counter_dict[color] += 1
+        else: color_counter_dict[color] = 1
     # To find the max.
-    for color in number_of_time_color_appeared:
-        if number_of_time_color_appeared > max_number:
-            max_number = number_of_time_color_appeared[color]
+    max_number: int = 0 
+    most_popular_color: str = ""  
+    for color in colors_list:
+        if color_counter_dict[color] > max_number:
             most_popular_color = color
+            max_number = color_counter_dict[color]
     return most_popular_color
 
 
 def count(input: list[str]) -> dict[str, int]:
     """Count of the number of times that a value appeared in the input list."""
     # Create an empty dictionary to store built-up result.
-    final_list: dict[str, int] = ()
+    final_list: dict[str, int] = dict()
     # Iterate over each item in input list.
-    for str in input:
+    for str_value in input:
         # Check to see if that item has already been established as a key in your dictionary.
-        if str in final_list:
+        if str_value in final_list:
             # Item is found in the dict- means there is already a key/value pair where the item is a key. 
-            final_list[str] += 1
+            final_list[str_value] += 1
         # Item is not found in the dict, that means this is the first time you are encountering the value. 
-        else: final_list[str] = 1
+        else: final_list[str_value] = 1
     # Return resulting dictionary. 
     return final_list
 
@@ -69,12 +67,13 @@ def alphabetizer(input: list[str]) -> dict[str, list[str]]:
         if letter in order_words:
                 order_words[letter].append(word)
         else: order_words[letter] = [word]
+    return order_words
 
 
 def update_attendance(list_attendance: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
     """Updates the attendance log."""
     # Updates the attendance list with the new attendance information, then return it. 
     if day in list_attendance:
-        list_attendance[day].append[student]
+        list_attendance[day].append(student)
     else: list_attendance[day] = [student]
     return list_attendance
